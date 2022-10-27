c9edb6cead90d4baa8c797f9e64f765ab4d57997
udeaghad <dozie.udeagha@gmail.com>
2022-10-27 13:56:19 +0100

    Create association for reservation

--- app/models/reservaton.rb
+++ app/models/reservaton.rb
@@ -1,2 +1,8 @@
 class Reservaton < ApplicationRecord
+  belongs_to :user, class_name: "User"
+  belongs_to :hotel, class_name: "Hotel"
+
+  validates :user_id, presence: true
+  validates :hotel_id, presence: true
+
 end
