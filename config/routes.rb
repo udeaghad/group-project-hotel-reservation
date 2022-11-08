0412e690d3a834d9643ab2b83df4bc38d841b8de
Kweku Bright <kweku01bright@gmail.com>
2022-11-08 16:19:27 +0100

    Implement homepage

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do 
+      resources :hotels, only: [:index]
       resources :users do     
         resources :hotels, only: [:index, :show] do 
           resources :reservations, only: [:create, :destroy]  
